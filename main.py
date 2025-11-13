c9eee62d5f4b662701edb28b0c49e3409c17bc55
lucas.giraud <lucasgiraud62@gmail.com>
2025-11-13 10:24:05 +0000

    Teste si un mot est un palimdrome

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+"""Programme qui permet de tester si un mot est un palindrome"""
+
 #### Fonction secondaire
-
-
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """Renvoir True ou False si la chaine en paramètre est un palindrome"""
+    dico = str.maketrans({"ë": "e","é": "e","è": "e", "É": "e",
+                          "ê": "e", "à": "a", "À": "a", ",": "",
+                           "'": "", "!": "", "?": "", " ": "",
+                         "ô": "o", "-": "", ":": "", "ç": "c"})
+    chaine = p.translate(dico)
+    final = chaine.lower()
+    if final[::]==final[::-1]:
+        return True
     return False
 
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """Fonction principale qui appel la fonction secondaire isplalindrome"""
+    print(ispalindrome("échec"))
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
